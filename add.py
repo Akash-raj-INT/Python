e9fc4a74775d91c35d8d9309c7d5f60426a3d5f6
Akash raj <akashraj848114@gmail.com>
2022-10-01 16:46:12 +0530

    addtion programm

---
+++ add.py
@@ -0,0 +1,11 @@
+#create two variable (x and y) and assign to them
+
+x = 5 
+y = 6
+sum = x+y
+print("the answer is : " ,sum)
+x = input ("Enter first number: ")
+y = input("Enter second number: ")
+sum = int(x)+int(y)
+print(sum)
+
