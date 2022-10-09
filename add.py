36119b5c2ef0fcc6257c1e6fb3e50df5585ee199
Akash raj <akashraj848114@gmail.com>
2022-10-09 22:32:35 +0530

    add.py

--- add.py
+++ add.py
@@ -2,10 +2,10 @@
 
 x = 5 
 y = 6
-sum = x+y
+sum = x + y
 print("the answer is : " ,sum)
 x = input ("Enter first number: ")
 y = input("Enter second number: ")
-sum = int(x)+int(y)
-print(sum)
+
+
 
