822431387672cff7bbdafc280c26468c1b0bbbc7
Akash raj <akashraj848114@gmail.com>
2022-10-19 19:36:41 +0530

    ashasw

---
+++ New folder/nestedifElse.py/arrayPractice.py
@@ -0,0 +1,10 @@
+#from unicodedata import name
+
+
+sentence = " a quick brown fox jumps over the lazy dog"
+print(len(sentence))
+
+if "ox" in sentence:
+    print("yes! it is there")
+else:
+ print("No")
