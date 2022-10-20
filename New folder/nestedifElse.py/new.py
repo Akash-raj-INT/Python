b619b78742a6a49cdedf0fd236e6e7be95837e13
Akash raj <akashraj848114@gmail.com>
2022-10-20 21:18:00 +0530

    njewf

---
+++ New folder/nestedifElse.py/new.py
@@ -0,0 +1,13 @@
+a = 200
+if a < 500:
+    print("a is less than 500")
+    if(a > 500):
+        print("a is less than 300")
+    elif a == 200:
+       print("a is 200")
+    else:
+       print("nothing") 
+elif a < 200:
+    print("a is less than 200") 
+else:
+    print("else will print")      
