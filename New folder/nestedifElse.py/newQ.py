cf38f6f7457cd2c19e2a10e43a3d08908d18addc
Akash raj <akashraj848114@gmail.com>
2022-10-22 21:49:48 +0530

    ajsck

---
+++ New folder/nestedifElse.py/newQ.py
@@ -0,0 +1,10 @@
+#if the length of that input is gretar than 3 character add ing as prefix else write given.
+a = input("enter the name ")
+b =("ing")
+character= len(a)
+
+if character>3:
+    print(a+b)
+else:
+    print(a)    
+
