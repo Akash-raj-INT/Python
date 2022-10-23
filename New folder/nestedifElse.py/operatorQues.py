87e3b60de35c9c1a490f29dac5f2cb280436c43f
Akash raj <akashraj848114@gmail.com>
2022-10-23 12:29:37 +0530

    ssaj

---
+++ New folder/nestedifElse.py/operatorQues.py
@@ -0,0 +1,14 @@
+
+#take input for num1, num2, operator
+#print out the result
+from ast import operator
+
+
+a = int(input("enter num1:"))
+b = int(input("enter num2:"))
+c = 1(input("enter operator:"))
+
+if c == "+":
+    print(a+b)
+
+
