2eb46cee53a16abe8fe0cbd44d8629f5c1ad1f0b
Akash raj <akashraj848114@gmail.com>
2022-11-10 19:20:44 +0530

    fqwge

---
+++ H.W/loop.py
@@ -0,0 +1,9 @@
+#using a loop and .append() method append each item with a Dr. prefix to the list.
+
+list = ["Phil","Oz","Seuss","Dre"]
+x = ["Dr."]
+
+for i in list:
+    for j in x:
+        print(j,i) 
+
