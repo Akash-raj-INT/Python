527726febfc7693fbd3d1fd48944ffe16546af33
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2022-11-12 16:57:22 +0530

    dwqdq

---
+++ abc/list.py
@@ -0,0 +1,43 @@
+colors = ["Red","Blue","Red","Black","22"]
+cars=["TATA","NANO","ALTO","Jeep"]
+players=["Dhoni","Messi","Dravid"]
+numbers = [1, 2, 3, 4,]
+
+#numbers.sort()
+#print(numbers)
+
+
+#newList = [x.lower() for x in cars]
+#print(newList) 
+ 
+#newList =[x for x in cars if x !="TATA"]
+#print(newList)
+
+
+
+
+
+#colors[0:2]= ["Yellow","white"] use for change and print selected elements
+#print(colors[1:4]) = use for print selected elements
+#colors.insert(2,"Orange") = use for adding elements
+#colors.append("BMW")      = use for adding elemets      
+#colors.extend (players)   = use for removing elements
+#colors.extend(cars)       =   ^^^^^^^
+#colors.pop(2)  = use for remove elements 
+#del colors[0]  =      ^^^^^^^^
+#colors.clear()  
+#for x in cars:
+#for x in range(len(cars)):
+    #print(cars [x])
+
+#while x < len(cars):    
+    #print(cars[x])
+    #x+=1
+
+#[print(x) for x in cars]
+
+#newList = []
+#or i in cars:
+   # if "A" in i:
+    #    newList.append(i)
+#print(newList)
---
+++ abc/listofcar.py
@@ -0,0 +1,7 @@
+cars=["TATA","NANO","ALTO","Jeep"]
+
+newList = []
+for i in cars:
+    if "A" in i:
+        newList.append(i)
+print(newList)
