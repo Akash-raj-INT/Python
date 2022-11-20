5e4b696383d8caedf21d85e13b7fc8ac97cc439d
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2022-11-20 19:46:16 +0530

    egrgrg

---
+++ pratice/qusetionLoop.py
@@ -0,0 +1,14 @@
+#write a program to display only those numbers
+#from a list that satisfy following condition
+
+#number must be divisible by 5
+#if the number is greater than 150, then skip it ans move to nect number
+#if the number is greater than 500, stop yje loop
+#print the above list in reverse order 
+#number = [12, 75, 150, 180, 145]
+num =[12, 75, 150, 180, 145]
+
+for i in num:
+    if i <150:
+        continue
+    print(i)
