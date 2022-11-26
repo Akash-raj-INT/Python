b1d82874d9a8a35fd00f5e59bd5df1b356ad9c84
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2022-11-26 21:48:25 +0530

    efws

---
+++ list/list.py
@@ -0,0 +1,52 @@
+colors = ["Red","Blue","Red","Black","22"]
+cars=["TATA","NANO","ALTO","Jeep"]
+players=["Dhoni","Messi","Dravid"]
+numbers = [1, 2, 3, 4,]
+
+list1 = ["x", "y", "z"]
+list2 = [1,2,3]
+
+list1.append(list2)
+print(list1)
+
+#num2 =numbers.copy()
+#print(num2)
+
+#numbers.sort()
+#print(numbers)
+
+
+#newList = [x.lower() for x in cars]
+#print(newList) 
+ 
+#newList =[x for x in cars if x !="TATA"]
+#print(newList)
+
+
+
+
+
+#colors[0:2]= ["Yellow","white"] use for change and print selected elements
+#print(colors[1:4]) = use for print selected elements
+#colors.insert(2,"Orange") = use for adding elements
+#colors.append("BMW")      = use for adding elemets      
+#colors.extend (players)   = use for removing elements
+#colors.extend(cars)       =   ^^^^^^^
+#colors.pop(2)  = use for remove elements 
+#del colors[0]  =      ^^^^^^^^
+#colors.clear()  
+#for x in cars:
+#for x in range(len(cars)):
+    #print(cars [x])
+
+#while x < len(cars):    
+    #print(cars[x])
+    #x+=1
+
+#[print(x) for x in cars]
+
+#newList = []
+#or i in cars:
+   # if "A" in i:
+    #    newList.append(i)
+#print(newList)
