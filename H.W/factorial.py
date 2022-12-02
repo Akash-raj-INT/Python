4d07a48967e05d4269d143ea8bdbd719d40764ce
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2022-12-02 18:39:31 +0530

    Add files via upload

---
+++ H.W/factorial.py
@@ -0,0 +1,15 @@
+int main() 
+
+
+
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= n; ++i) {
+            fact *= i;
+        }
+        printf("Factorial of %d = %llu", n, fact);
+    }
+
+    return 0;
+}
