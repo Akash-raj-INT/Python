b86555b76d7c42cf51b2ccfdc61a04ec153a185b
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2022-12-26 22:41:44 +0530

    Add files via upload

---
+++ string/elif.py
@@ -0,0 +1,21 @@
+# a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+
+
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+
+#   a = 2
+# b = 330
+# print("A") if a > b else print("B")
