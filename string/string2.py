c220623f8be5ad8b25b2e8b7a676682556ac6983
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2022-12-26 22:43:56 +0530

    string2

---
+++ string/string2.py
@@ -0,0 +1,15 @@
+# a = "Hello"
+# b = "World"
+# c = a + b
+# print(c)
+
+
+# a = "Hello"
+# b = "World"
+# c = a + " " + b
+# print(c)
+
+
+# age = 36
+# txt = "My name is John, and I am {}"
+# print(txt.format(age))
