2abf91638c1b67b03f71cf933e8fe2b7c1f9abaa
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2022-12-26 22:44:13 +0530

    string3

---
+++ string/string3.py
@@ -0,0 +1,11 @@
+# quantity = 3
+# itemno = 567
+# price = 49.95
+# myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+# print(myorder.format(quantity, itemno, price))
+
+
+# txt = "We are the so-called \"Vikings\" from the north."
+# print(txt) 
+
+
