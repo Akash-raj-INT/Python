a5d5771fc1a302d2b8c658da376972dd363a64c7
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2022-12-26 22:43:27 +0530

    string

---
+++ string/string.py
@@ -0,0 +1,18 @@
+# b = "Hello, World!"
+# print(b[2:5])
+
+
+# b = "Hello, World!"
+# print(b[2:5])
+
+# b = "Hello, World!"
+# print(b[2:5])
+
+
+# a = "Hello, World!"
+# b = a.split(",")
+# print(b)
+
+
+# a = "Hello, World!"
+# print(a.replace("H", "J"))
