dd8111e2856920078a8c6bc4fa74dfdc9cf7b134
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-01-06 22:49:34 +0530

    Add files via upload

---
+++ pyhonQn/code5.py
@@ -0,0 +1,16 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+x = cars[0]
+
+print(x)
+
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+p1 = Person("John", 36)
+
+print(p1.name)
+print(p1.age)
