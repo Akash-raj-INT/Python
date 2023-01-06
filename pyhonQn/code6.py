820281f35d05d7cb33eda1988568f1360fae000d
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-01-06 22:49:50 +0530

    Add files via upload

---
+++ pyhonQn/code6.py
@@ -0,0 +1,16 @@
+a = 200
+b = 33
+if b < a:
+  print("a is greater than b")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is not greater than b")
+
+
+
+a = 200
+b = 33
+c = 500
+if a > b and c > a:
+  print("Both conditions are True")
