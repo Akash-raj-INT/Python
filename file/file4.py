d39dbf594b606146cb84a456529fb6785805fe62
Akash raj <114800476+Akash-raj-INT@users.noreply.github.com>
2023-01-31 22:02:25 +0530

    Add files via upload

---
+++ file/file4.py
@@ -0,0 +1,13 @@
+f = open("demofile.txt", "r")
+
+print(f.readline())
+
+f.close()
+
+f = open("demofile2.txt", "a")
+f.write("Now the file has more content!")
+f.close()
+
+#open and read the file after the appending:
+f = open("demofile2.txt", "r")
+print(f.read())
